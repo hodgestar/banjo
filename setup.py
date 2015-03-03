40b910d450ab9f00837534cce85c31e959a07a64
Simon Cross <hodgestar@gmail.com>
2015-03-03 22:45:27 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="banjo",
+    version="0.0.1",
+    url='http://github.com/hodgestar/banjo',
+    license='MIT',
+    description="Banjo Hero in Pygame, a tutorial.",
+    long_description=open('README.rst', 'r').read(),
+    author='Simon Cross',
+    author_email='hodgestar@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pygame'
+        'wiring',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
+)
