4e5fdab41233d4a778a0ea4141827b115ba7755e
Simon Cross <hodgestar@gmail.com>
2015-03-03 23:15:49 +0200

    Skeleton of a game engine.

---
+++ banjo/engine.py
@@ -0,0 +1,16 @@
+""" Banjo game loop. """
+
+from __future__ import absolute_import
+
+
+class Engine(object):
+    """ Banjo's game engine. """
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def run(self):
+        print "Twang."
--- banjo/scripts/banjo.py
+++ banjo/scripts/banjo.py
@@ -1,12 +1,20 @@
 """ Launch Banjo Hero. """
 
+from __future__ import absolute_import
+
 import click
+from banjo.engine import Engine
 
 
 @click.command()
 def cli():
     """ Banjo Hero in Pygame. """
-    print "Twang."
+    engine = Engine()
+    engine.setup()
+    try:
+        engine.run()
+    finally:
+        engine.teardown()
 
 
 if __name__ == "__main__":
