b8900b99b1882581288fba2dd54264c1fa323549
Simon Cross <hodgestar@gmail.com>
2015-03-05 00:32:36 +0200

    Document the plan for the game loop.

--- banjo/engine.py
+++ banjo/engine.py
@@ -1,4 +1,26 @@
-""" Banjo game loop. """
+""" Banjo game loop.
+
+A game loop may be seen as a function ``f: W_i -> W_i+1`` that updates the
+World (W_i) from time t_i to time t_i+1.
+
+The World at t_i may be broken down into:
+
+  * a screen to display images on (D_i)
+  * speakers to play music via (M_i)
+  * the game state (G_i)
+  * the current scene within the game (S_i)
+
+Given a set of Pygame events PE_i+1 that have occurred between t_i and t_i+1
+we define ``f`` using:
+
+  * S_i+1, SE_i+1 = S_i.update(G_i, PE_i+1)
+  * G_i+1 = G_i.apply_state_events(SE_i+1)
+  * M_i+1 = M_i.apply_music_events(SE_i+1)
+  * D_i+1 = S_i+1.render(G_i+1)
+
+where SE_i+1 is a list of scene events generated in the transition from S_i
+to S_i+1.
+"""
 
 from __future__ import absolute_import
 
